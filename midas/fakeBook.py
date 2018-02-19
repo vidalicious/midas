b14b0dbc73182b527ea496ee7edeabd49457802b
vidalicious <vidalicious@sina.com>
2018-02-19 19:19:58 +0800

    dev

--- midas/fakeBook.py
+++ midas/fakeBook.py
@@ -4,7 +4,11 @@ import time
 from functools import reduce
 from pandas import DataFrame
 
-COL_PAST5 = 'past_5_p_change'
+COL_PASTCHANGE = 'past_p_change'
+COL_PASTPOSITIVE = 'past_positive'
+PAST_DAY_PERIOD = 10
+PAST_POSITIVE_PERIOD = 3
+
 
 def past_hist_p_change(histData, pastDay=5):
     past_p_changes = histData['p_change'][:pastDay]
@@ -17,26 +21,30 @@ def _main():
 
     frame = DataFrame()
     frame['name'] = names
-    frame[COL_PAST5] = None
+    frame[COL_PASTCHANGE] = None
+    frame[COL_PASTPOSITIVE] = None
     i = 0
     for code in names.index:
         histData = ts.get_hist_data(code)
         try:
-            frame[COL_PAST5][code] = past_hist_p_change(histData, 5)
+            frame[COL_PASTCHANGE][code] = past_hist_p_change(histData, PAST_DAY_PERIOD)
+            if past_hist_p_change(histData, PAST_POSITIVE_PERIOD) > 0:
+                frame[COL_PASTPOSITIVE][code] = '★'
         except Exception:
             continue
 
         i += 1
         print('#####', i, '#####')
 
-    sortedFrame = frame.sort_values(by=COL_PAST5)
+    sortedFrame = frame.sort_values(by=COL_PASTCHANGE)
     print(sortedFrame)
 
     t = time.strftime('%Y-%m-%d', time.localtime())
-    fileName = '../logs/%s%s' %(t, '.csv')
+    fileName = '../logs/%s_%s%s' %(t, PAST_DAY_PERIOD, '.csv')
     # print(fileName)
     with open(fileName, 'w', encoding='utf8') as file:
         sortedFrame.to_csv(file)
+
 
 if __name__=='__main__':
     _main()
--- midas/test.py
+++ midas/test.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import tushare as ts
 
-histData = ts.get_hist_data('002069')
+histData = ts.get_hist_data('603637')
 print(histData)
+
+# print(1.1 ** 28)
