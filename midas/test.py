278445b6af6fca2ef399102f477227ed33182396
vidalicious <vidalicious@sina.com>
2018-11-18 00:02:16 +0800

    dev

--- midas/test.py
+++ midas/test.py
@@ -3,8 +3,12 @@ import tushare as ts
 import midas.midas.api as api
 import numpy as np
 
-histData = ts.get_hist_data('002930')
-print(histData)
+hist_data = ts.get_hist_data('002575')
+a = api.hist_p_change(hist_data, begin=24, end=34)
+b = api.normalizing_std_close(hist_data, begin=24, end=34)
+print(a)
+print(b)
+# print(histData)
 # print(api.normalizing_std_close(histData, 1, 11))
 # print([1, 2, 3, 5][:2])
 # print(1.1 ** 28)
@@ -15,3 +19,8 @@ print(histData)
 # print(np.std(h) / np.mean(h))
 #
 # print((1.2**5 - np.mean(h)) / np.std(h))
+
+# basics = ts.get_stock_basics()
+#
+#
+# print(basics)
