a65b71fe054fcf38e8a9b46d623ca0692c8c692d
vidalicious <vida.gu@cootek.cn>
2018-11-22 15:57:51 +0800

    dev

---
+++ midas/runner.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import midas.midas._14segment as segment
+import midas.midas._15observer as observer
+import midas.midas._16ladder as ladder
+
+
+if __name__ == '__main__':
+    segment._main()
+    observer._main()
+    ladder._main()
+
