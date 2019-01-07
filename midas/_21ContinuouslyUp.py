4d5720b04434187c3451d55089c3c42b529eeec3
vidalicious <vidalicious@sina.com>
2019-01-08 00:44:24 +0800

    dev

--- midas/_21ContinuouslyUp.py
+++ midas/_21ContinuouslyUp.py
@@ -61,8 +61,8 @@ def main():
     data_frame = data_frame[
                            (data_frame['circ_mv'] < 1000000)
                            & (data_frame[COL_CONTINUOUSLY_UP] > 1)
-                           # & (data_frame[COL_AVERAGE_TURNOVER] < 5)
-                           & (data_frame[COL_ACCUMULATE_P_CHANGE] > 5)
+                           & (data_frame[COL_AVERAGE_P_CHANGE] > 5)
+                           # & (data_frame[COL_ACCUMULATE_P_CHANGE] > 5)
                            & (data_frame[COL_PRE_ACCUMULATE_P_CHANGE] < data_frame[COL_ACCUMULATE_P_CHANGE])
                            & (data_frame[COL_LAST_MA20_GAP] > 0)
                            ]
