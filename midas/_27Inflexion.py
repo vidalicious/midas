41a3362b1cd2c185dc6c8cdc5a005ed2ce7ae9e4
vidalicious <vidalicious@sina.com>
2019-01-20 21:58:04 +0800

    dev

--- midas/_27Inflexion.py
+++ midas/_27Inflexion.py
@@ -9,9 +9,8 @@ import numpy as np
 import midas.midas.api_pro as api
 
 COL_WEIGHT_CONTINUOUSLY_FALL_P_CHANGE = 'WEIGHT_CONTINUOUSLY_FALL_P_CHANGE'
-COL_WEIGHT_LAST_P_CHANGE = 'WEIGHT_LAST_P_CHANGE'
-COL_INFLEXION = 'INFLEXION'
-COL_WEEKLY_WEIGHT_LAST_P_CHANGE = 'WEEKLY_WEIGHT_LAST_P_CHANGE'
+COL_INDAY_P_CHANGE = 'INDAY_P_CHANGE'
+COL_WEEKLY_CONTINUOUSLY_UP_COUNT = 'WEEKLY_CONTINUOUSLY_UP_COUNT'
 
 sampling_count = 100
 
@@ -20,7 +19,7 @@ def main():
     pro = ts.pro_api()
 
     trade_dates = pro.daily(ts_code='000001.SZ').trade_date
-    LAST_MARKET_DATE = trade_dates[0]
+    LAST_MARKET_DATE = trade_dates[1]
 
     stock_basic = pro.stock_basic(list_status='L', fields='ts_code,symbol,name,industry,fullname')
 
@@ -28,19 +27,18 @@ def main():
     for key in ['ts_code', 'name', 'industry']:
         data_frame[key] = stock_basic[key]
 
-    for key in [COL_WEIGHT_CONTINUOUSLY_FALL_P_CHANGE, COL_WEIGHT_LAST_P_CHANGE, COL_INFLEXION,
-                COL_WEEKLY_WEIGHT_LAST_P_CHANGE]:
+    for key in [COL_WEIGHT_CONTINUOUSLY_FALL_P_CHANGE, COL_INDAY_P_CHANGE,
+                COL_WEEKLY_CONTINUOUSLY_UP_COUNT]:
         data_frame[key] = np.nan
 
     for i, ts_code in enumerate(data_frame.ts_code):
         try:
             daily = pro.daily(ts_code=ts_code, start_date=trade_dates[sampling_count], end_date=LAST_MARKET_DATE)
             data_frame.loc[i, COL_WEIGHT_CONTINUOUSLY_FALL_P_CHANGE] = api.daily_weight_continuously_fall_p_change(daily=daily, begin=1)
-            data_frame.loc[i, COL_WEIGHT_LAST_P_CHANGE] = api.daily_weight_p_change(daily=daily, begin=0, end=1)
-            data_frame.loc[i, COL_INFLEXION] = round(data_frame.loc[i, COL_WEIGHT_LAST_P_CHANGE] - data_frame.loc[i, COL_WEIGHT_CONTINUOUSLY_FALL_P_CHANGE], 2)
+            data_frame.loc[i, COL_INDAY_P_CHANGE] = api.daily_inday_p_change(daily=daily, index=0)
             weekly = pro.weekly(ts_code=ts_code,          #trade_date=LAST_MARKET_DATE
                                 start_date=trade_dates[sampling_count], end_date=LAST_MARKET_DATE)
-            data_frame.loc[i, COL_WEEKLY_WEIGHT_LAST_P_CHANGE] = api.weekly_weight_p_change(weekly=weekly, begin=0, end=1)
+            data_frame.loc[i, COL_WEEKLY_CONTINUOUSLY_UP_COUNT] = api.weekly_continuously_weight_up_count(weekly=weekly)
         except Exception as e:
             print('excetion in {}'.format(i))
             continue
@@ -50,11 +48,10 @@ def main():
     data_frame = data_frame[
                            # (data_frame['circ_mv'] < 1000000)
                            (data_frame[COL_WEIGHT_CONTINUOUSLY_FALL_P_CHANGE] < 0)
-                           & (data_frame[COL_WEIGHT_LAST_P_CHANGE] > 0)
-                           & (data_frame[COL_WEEKLY_WEIGHT_LAST_P_CHANGE] > 0)
+                           & (data_frame[COL_INDAY_P_CHANGE] > 0)
                            ]
 
-    sorted_frame = data_frame.sort_values(by=COL_WEIGHT_CONTINUOUSLY_FALL_P_CHANGE, ascending=True)
+    sorted_frame = data_frame.sort_values(by=COL_WEEKLY_CONTINUOUSLY_UP_COUNT, ascending=False)
 
     file_name = '../logs/{date}@Inflexion.csv'.format(date=LAST_MARKET_DATE)
     # print(fileName)
