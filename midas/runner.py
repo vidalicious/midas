07e17a92ae7148a409e0a5a4f3869959f39c5e2e
vidalicious <vidalicious@sina.com>
2019-02-19 23:13:49 +0800

    dev

--- midas/runner.py
+++ midas/runner.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import midas.midas._24HeavyChaser as chaser
-import midas.midas._25WeeklyTrend as trend
+import midas.midas._22FallBreaker as fallbreaker
+import midas.midas._29FreeFall as freefall
 
 
 if __name__ == '__main__':
-    trend.main()
     chaser.main()
+    freefall.main()
+    fallbreaker.main()
