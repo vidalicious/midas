c2ae53107f097079b554571bc7e3187272dff104
vidalicious <vidalicious@sina.com>
2019-03-14 00:38:06 +0800

    dev

--- midas/runner.py
+++ midas/runner.py
@@ -2,8 +2,10 @@
 
 import midas.midas._24HeavyChaser as chaser
 import midas.midas._31DeepSearch as deep
+import midas.midas._33RiseTempo as tempo
 
 
 if __name__ == '__main__':
     deep.main()
+    tempo.main()
     chaser.main()
