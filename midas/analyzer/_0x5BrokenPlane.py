381c7dab9e9f5e0bd71e4b690761a823ad959288
vidalicious <vida.gu@cootek.cn>
2019-03-29 15:18:22 +0800

    dev

---
+++ midas/analyzer/_0x5BrokenPlane.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import json
+import time
+
+import tushare as ts
+from pandas import DataFrame
+import numpy as np
+
+import midas.midas.analyzer.api as api
+
+import midas.midas.data.models as models
+from midas.midas.data.engine import main_session
+
+COL_LIMIT_PERIOD = 'COL_LIMIT_PERIOD'
+COL_LAST_PCT_CHG = 'COL_LAST_PCT_CHG'
+COL_LASTPRICE = 'COL_LASTPRICE'
+
+sampling_count = 40
+
+
+def main(offset=0):
+    daily001 = main_session.query(models.DailyPro).filter(models.DailyPro.ts_code == '000001.SZ').order_by(models.DailyPro.trade_date.desc()).all()
+    LAST_MARKET_DATE = daily001[offset].trade_date
+
+    data_frame = DataFrame()
+    for i, stock_basic in enumerate(main_session.query(models.StockBasicPro).all()):
+        try:
+            for key in models.StockBasicPro.keys:
+                data_frame.loc[i, key] = getattr(stock_basic, key)
+
+            daily = main_session.query(models.DailyPro).filter(models.DailyPro.ts_code == stock_basic.ts_code,
+                                                               models.DailyPro.trade_date <= LAST_MARKET_DATE).order_by(
+                models.DailyPro.trade_date.desc()).limit(sampling_count).all()
+
+            data_frame.loc[i, COL_LIMIT_PERIOD] = api.daily_limit_period(daily=daily, begin=0, end=15)
+            data_frame.loc[i, COL_LAST_PCT_CHG] = daily[0].pct_chg
+            data_frame.loc[i, COL_LASTPRICE] = daily[0].close
+        except Exception as e:
+            print('excetion in index:{index} {code} {name}'.format(index=i, code=stock_basic.ts_code, name=stock_basic.name))
+            continue
+        print('##### {i} #####'.format(i=i))
+
+    data_frame = data_frame[
+                            (data_frame[COL_LAST_PCT_CHG] > 7)
+                           ]
+
+    sorted_frame = data_frame.sort_values(by=COL_LIMIT_PERIOD, ascending=False).reset_index(drop=True)
+
+    file_name = '../../logs/{date}@BrokenPlane.csv'.format(date=LAST_MARKET_DATE)
+    # print(fileName)
+    with open(file_name, 'w', encoding='utf8') as file:
+        sorted_frame.to_csv(file)
+    return sorted_frame
+
+
+if __name__ == '__main__':
+    main()
--- midas/analyzer/api.py
+++ midas/analyzer/api.py
@@ -38,6 +38,27 @@ def daily_weight_rise_efficiency(daily=None, begin=0, end=1):
     return (average_weight_rise, min_index, max_index)
 
 
+def daily_limit_period(daily=None, begin=0, end=1):
+    if begin == end:
+        return 0
+
+    limit_begin = 9999
+    for i in range(begin, end):
+        if daily[i].pct_chg > 9.98:
+            limit_begin = i
+            break
+
+    limit_end = -1
+    for i in range(end, begin, -1):
+        if daily[i].pct_chg > 9.98:
+            limit_end = i
+            break
+
+    if limit_begin <= limit_end:
+        return limit_end - limit_begin + 1
+    return 0
+
+
 def daily_weight_exponential_fitness(daily=None, begin=0, end=1, exp=2):
     if begin == end:
         return 0
