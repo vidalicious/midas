894754aeaf8b7636f2bc35f4cbc75804a1b25e25
vidalicious <vida.gu@cootek.cn>
2019-04-17 20:15:20 +0800

    dev

---
+++ midas/data/async_all.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import midas.midas.data.async as async
+import midas.midas.data.async_concept as concept
+import midas.midas.data.async_stock_company as company
+
+concept.main()
+company.main()
+async.main()
