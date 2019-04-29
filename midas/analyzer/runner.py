a0d06c495b60c3ca6a96ca6c96fc4a35c16baa01
vidalicious <vidalicious@sina.com>
2019-04-30 02:59:13 +0800

    dev

--- midas/analyzer/runner.py
+++ midas/analyzer/runner.py
@@ -4,11 +4,11 @@ import midas.midas.analyzer._0x2Regression_exp1 as reg1
 import midas.midas.analyzer._0x5BrokenPlane as broken
 import midas.midas.analyzer._0x4RiseTempo as tempo
 import midas.midas.analyzer._0x10Close_ma_dive as closema
+import midas.midas.analyzer._0x11MA_walker as walker
 
 if __name__ == '__main__':
     reg.main()
     reg1.main()
-    closema.main(10)
-    closema.main(20)
+    walker.main(0)
     broken.main()
     tempo.main()
