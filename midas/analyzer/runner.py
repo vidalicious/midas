ea9f35502c4072b73a47b39f7f57d2d0ded2599b
vidalicious <vida.gu@cootek.cn>
2019-05-17 15:22:52 +0800

    dev

--- midas/analyzer/runner.py
+++ midas/analyzer/runner.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import midas.midas.analyzer._0x1Regression as reg
 import midas.midas.analyzer._0x2Regression_exp1 as reg1
-import midas.midas.analyzer._0x12MA_10_20 as ma1020
-import midas.midas.analyzer._0x16DailyLimitLowPrice as dllp
+# import midas.midas.analyzer._0x12MA_10_20 as ma1020
+# import midas.legacy._0x16DailyLimitLowPrice as dllp
 import midas.midas.analyzer._0x17MA_diffuse as diffuse
 import midas.midas.analyzer._0x18Atmosphere as atmosphere
 import midas.midas.analyzer._0x19ContinuousLimit as continuous
@@ -10,8 +10,6 @@ import midas.midas.analyzer._0x19ContinuousLimit as continuous
 if __name__ == '__main__':
     reg.main()
     reg1.main()
-    ma1020.main()
-    dllp.main()
     diffuse.main()
     atmosphere.main()
     continuous.main()
