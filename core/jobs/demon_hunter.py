0956478376edc1a9a56a54194dcc38beeb960894
vidalicious <weida.gu@yintech.cn>
2020-04-30 12:04:12 +0800

    dev

--- core/jobs/demon_hunter.py
+++ core/jobs/demon_hunter.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import midas.core.data.async_base as base
-# import midas.core.data.async_weekly as weekly
+import midas.core.data.async_weekly as weekly
 import midas.core.data.async_float_holders as holders
 import midas.core.analyzer._0x22DemonHunter as demon_hunter
 import midas.core.analyzer._0x26WindChime as wind_chime
 import midas.core.analyzer._0x25TwoPlus as two_plus
 import midas.core.analyzer._0x24Drager_List as drager
+import midas.core.analyzer._0x27WeeklyTidal as tidal
 
 
 def working_day():
@@ -19,9 +20,10 @@ def working_day():
 
 
 def weekend():
-    # weekly.async_weekly()
+    weekly.async_weekly()
     holders.async_float_holders()
     demon_hunter.main()
+    tidal.main()
     wind_chime.main()
     two_plus.main()
     drager.main()
@@ -30,9 +32,10 @@ def weekend():
 def total():
     base.async_stock_basic()
     base.async_daily()
-    # weekly.async_weekly()
+    weekly.async_weekly()
     holders.async_float_holders()
     demon_hunter.main()
+    tidal.main()
     wind_chime.main()
     two_plus.main()
     drager.main()
