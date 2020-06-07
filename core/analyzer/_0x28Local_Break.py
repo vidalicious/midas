71a3f069361f571f4b364d104bd8b34d2d54b1d8
vidalicious <vidalicious@sina.com>
2020-06-07 12:45:27 +0800

    dev

--- core/analyzer/_0x28Local_Break.py
+++ core/analyzer/_0x28Local_Break.py
@@ -5,12 +5,16 @@ import time
 import tushare as ts
 from pandas import DataFrame
 import numpy as np
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 import midas.core.analyzer.api as api
 
 import midas.core.data.models as models
 from midas.core.data.engine import main_session
 import midas.bin.env as env
+import shutil
+import os
 
 
 COL_DAILY_BREAK = 'COL_DAILY_BREAK'
@@ -63,6 +67,13 @@ def main(offset=0):
     with open(file_name, 'w', encoding='utf8') as file:
         data_frame.to_csv(file)
 
+    shutil.rmtree('../../buffer/local_break/local_break')
+    os.mkdir('../../buffer/local_break/local_break')
+    for i in range(len(data_frame)):
+        ts_code = data_frame.loc[i, 'ts_code']
+        name = data_frame.loc[i, 'name']
+        plot(ts_code=ts_code, name=name, last_date=LAST_MARKET_DATE, doc='local_break', file_prefix=i)
+
     data_frame = data_frame[
                             (data_frame[COL_DAILY_BREAK_OFFSET1] == False)
                            ]
@@ -70,6 +81,30 @@ def main(offset=0):
     file_name = '{logs_path}/{date}@Local_Break_differ.csv'.format(date=LAST_MARKET_DATE, logs_path=env.logs_path)
     with open(file_name, 'w', encoding='utf8') as file:
         data_frame.to_csv(file)
+
+    shutil.rmtree('../../buffer/local_break/local_break_differ')
+    os.mkdir('../../buffer/local_break/local_break_differ')
+    for i in range(len(data_frame)):
+        ts_code = data_frame.loc[i, 'ts_code']
+        name = data_frame.loc[i, 'name']
+        plot(ts_code=ts_code, name=name, last_date=LAST_MARKET_DATE, doc='local_break_differ', file_prefix=i)
+
+
+def plot(ts_code, name, last_date, doc, file_prefix=0):
+    sns.set(style="whitegrid")
+
+    daily = main_session.query(models.DailyPro).filter(models.DailyPro.ts_code == ts_code,
+                                                       models.DailyPro.trade_date <= last_date).order_by(
+        models.DailyPro.trade_date.desc()).limit(sampling_count).all()
+
+    data = [item.close for item in daily][120::-1]
+    data =  pd.DataFrame(data, columns=[ts_code])
+
+    sns.lineplot(data=data, palette="tab10", linewidth=1.5).get_figure().savefig('../../buffer/local_break/{doc}/{prefix}_{ts_code}_{name}@{date}.png'
+        .format(doc=doc, ts_code=ts_code, name=name, date=last_date, prefix=file_prefix))
+    plt.clf()
+    print('plot {ts_code} {name}'.format(ts_code=ts_code, name=name))
+
 
 if __name__ == '__main__':
     main(offset=0)
