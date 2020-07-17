dcae1d6f5872ad304c32791a71063da400278944
vidalicious <weida.gu@yintech.cn>
2020-07-17 10:24:28 +0800

    dev

--- core/live/_0x1Violent_Move.py
+++ core/live/_0x1Violent_Move.py
@@ -32,6 +32,7 @@ def run():
         try:
             responses = grequests.map(req_list)
             print('====== {} ======'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+            local_breaks = []
             for response in responses:
                 res = response.text.strip().split(';\n')
                 for i in res:
@@ -45,11 +46,16 @@ def run():
                     local_max = stock_map[code]
                     if 0.05 < chg < 0.098:
                         if current_price > local_max:
-                            print('local break\t{code}\t{name}\tchg:{chg}\tprice:{price}'.format(code=code, name=name, chg=chg_display, price=round(current_price, 2)))
+                            local_breaks.append({
+                                'note': 'local break\t{code}\t{name}\tchg:{chg}\tprice:{price}'.format(code=code, name=name, chg=chg_display, price=round(current_price, 2)),
+                                'chg': chg
+                            })
                         else:
-                            print('violent move\t{code}\t{name}\tchg:{chg}\tprice:{price}'.format(code=code, name=name, chg=chg_display, price=round(current_price, 2)))
+                            # print('violent move\t{code}\t{name}\tchg:{chg}\tprice:{price}'.format(code=code, name=name, chg=chg_display, price=round(current_price, 2)))
                             pass
-                pass
+            local_breaks.sort(key=lambda x: x['chg'], reverse=False)
+            notes = [i['note'] for i in local_breaks]
+            print('\n'.join(notes))
         except Exception as e:
             print(e)
             continue
