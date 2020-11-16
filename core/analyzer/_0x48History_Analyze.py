f9d95d2ea69ef59c6f291874b8b71574d0be4043
vidalicious <weida.gu@yintech.cn>
2020-11-16 19:04:36 +0800

    dev

--- core/analyzer/_0x48History_Analyze.py
+++ core/analyzer/_0x48History_Analyze.py
@@ -17,16 +17,16 @@ import midas.bin.env as env
 import mpl_finance as mpf
 
 
-COL_LASTPRICE = 'COL_LASTPRICE'
-# COL_RECENT_LIMIT_COUNT_15 = 'COL_RECENT_LIMIT_COUNT_15'
-COL_RECENT_LIMIT_COUNT_3 = 'COL_RECENT_LIMIT_COUNT_3'
-COL_CONTINUOUS_LIMIT_COUNT = 'COL_CONTINUOUS_LIMIT_COUNT'
-COL_FLOAT_HOLDERS = 'COL_FLOAT_HOLDERS'
-COL_HOLDERS_COUNT = 'COL_HOLDERS_COUNT'
-COL_CIRC_MV = 'COL_CIRC_MV'
+COL_RECENT_LIMIT_COUNT_300 = 'COL_RECENT_LIMIT_COUNT_300'
 
-sampling_count = 300
+sampling_count = 301
 
+targets = [
+    '奥特佳', '星期六', '正川股份', '振德医疗', '海汽集团', '省广集团', '蓝英装备', '容大感光', '漫步者', '道恩股份', '秀强股份',
+    '宣亚国际', '新力金融', '西藏药业', '模塑科技', '中潜股份', '沈阳化工', '引力传媒', '北玻股份', '星徽精密', '凯撒旅业', '泰达股份', '掌阅科技',
+    '爱司凯', '天山生物', '搜于特', '南宁百货', '银之杰', '三五互联', '保龄宝', '君正集团', '金健米业', '经纬辉开', '通光线缆', '万通智控', '联环药业',
+    '凯撒文化', '奥美医疗', '光启技术', '海特生物', '智慧农业'
+]
 
 def main(offset=0):
     daily001 = main_session.query(models.DailyPro).filter(models.DailyPro.ts_code == '000001.SZ').order_by(models.DailyPro.trade_date.desc()).all()
@@ -34,6 +34,9 @@ def main(offset=0):
 
     data_frame = DataFrame()
     for i, stock_basic in enumerate(main_session.query(models.StockBasicPro).all()):
+        if not stock_basic.name in targets:
+            continue
+
         try:
             for key in models.StockBasicPro.keys:
                 data_frame.loc[i, key] = getattr(stock_basic, key)
@@ -41,40 +44,27 @@ def main(offset=0):
             daily = main_session.query(models.DailyPro).filter(models.DailyPro.ts_code == stock_basic.ts_code,
                                                                models.DailyPro.trade_date <= LAST_MARKET_DATE).order_by(
                 models.DailyPro.trade_date.desc()).limit(sampling_count).all()
-            data_frame.loc[i, COL_LASTPRICE] = daily[0].close
-            # data_frame.loc[i, COL_RECENT_LIMIT_COUNT_15] = api.local_limit_count(daily, local_scale=15)
-            data_frame.loc[i, COL_RECENT_LIMIT_COUNT_3] = api.local_limit_count(daily, local_scale=3)
-            data_frame.loc[i, COL_CONTINUOUS_LIMIT_COUNT] = api.daily_continuous_limit_count(daily)
-
-            daily_basic = main_session.query(models.DailyBasic).filter(models.DailyBasic.ts_code == stock_basic.ts_code).one()
-            data_frame.loc[i, COL_CIRC_MV] = daily_basic.circ_mv
-
-            holders = main_session.query(models.FloatHolderPro).filter(models.FloatHolderPro.ts_code == stock_basic.ts_code).all()
-            h_set = set()
-            for item in holders:
-                h_set.add(item.holder_name)
-            data_frame.loc[i, COL_FLOAT_HOLDERS] = '\n'.join(h_set)
-            data_frame.loc[i, COL_HOLDERS_COUNT] = len(h_set)
+            data_frame.loc[i, COL_RECENT_LIMIT_COUNT_300] = api.local_limit_count(daily, local_scale=300)
 
         except Exception as e:
             print('exception in index:{index} {code} {name}'.format(index=i, code=stock_basic.ts_code, name=stock_basic.name))
             continue
-        print('##### limit_rank {i} #####'.format(i=i))
+        print('##### history_analyze {i} #####'.format(i=i))
 
     data_frame = data_frame[
                             # (data_frame[COL_RECENT_LIMIT_COUNT_15] > 1)
                             # | ((data_frame[COL_RECENT_LIMIT_COUNT_15] == 1) & (data_frame[COL_RECENT_LIMIT_COUNT_3] > 0))
-                            (data_frame[COL_RECENT_LIMIT_COUNT_3] > 0)
+                            (data_frame[COL_RECENT_LIMIT_COUNT_300] > 5)
                            ]
 
-    data_frame = data_frame.sort_values(by=COL_CONTINUOUS_LIMIT_COUNT, ascending=False).reset_index(drop=True)
+    data_frame = data_frame.sort_values(by=COL_RECENT_LIMIT_COUNT_300, ascending=False).reset_index(drop=True)
     # data_frame = data_frame.loc[:, ['ts_code', 'name', 'industry', COL_LASTPRICE, COL_FLOAT_HOLDERS]]
 
-    file_name = '{logs_path}/{date}@Limit_Rank.csv'.format(date=LAST_MARKET_DATE, logs_path=env.logs_path)
+    file_name = '{logs_path}/{date}@History_Analyze.csv'.format(date=LAST_MARKET_DATE, logs_path=env.logs_path)
     with open(file_name, 'w', encoding='utf8') as file:
         data_frame.to_csv(file)
 
-    batch_size = 100
+    batch_size = 200
     sub = 0
     for i in range(0, len(data_frame), batch_size):
         sub_df = data_frame.iloc[i:i+batch_size, :]
@@ -92,17 +82,12 @@ def plot_candle_gather(data_frame, last_date, sub):
         ts_code = data_frame.loc[i, 'ts_code']
         name = data_frame.loc[i, 'name']
         ax = fig.add_subplot(rows, columns, i + 1)
-        misc = {
-            COL_HOLDERS_COUNT: data_frame.loc[i, COL_HOLDERS_COUNT] if not np.isnan(data_frame.loc[i, COL_HOLDERS_COUNT]) else 0,
-            COL_CIRC_MV: data_frame.loc[i, COL_CIRC_MV] if not np.isnan(data_frame.loc[i, COL_CIRC_MV]) else 0,
-            COL_CONTINUOUS_LIMIT_COUNT: data_frame.loc[i, COL_CONTINUOUS_LIMIT_COUNT]
-        }
-        plot_candle(ax=ax, ts_code=ts_code, name=name, last_date=last_date, misc=misc)
+        plot_candle(ax=ax, ts_code=ts_code, name=name, last_date=last_date)
 
     plt.tight_layout()
-    plt.savefig('../../buffer/limit_rank/{date}_limit_rank_{sub}.png'.format(date=last_date, sub=sub))
+    plt.savefig('../../buffer/history_analyze/{date}_history_analyze_{sub}.png'.format(date=last_date, sub=sub))
 
-def plot_candle(ax, ts_code, name, last_date, misc):
+def plot_candle(ax, ts_code, name, last_date):
     daily = main_session.query(models.DailyPro).filter(models.DailyPro.ts_code == ts_code,
                                                        models.DailyPro.trade_date <= last_date).order_by(
         models.DailyPro.trade_date.desc()).limit(sampling_count).all()
@@ -125,8 +110,7 @@ def plot_candle(ax, ts_code, name, last_date, misc):
     ax.plot(sma_10, linewidth=1, label='ma10')
     ax.plot(sma_20, linewidth=1, label='ma20')
 
-    plt.title('{ts_code} {name} circ_mv:{circ_mv}亿 holders:{holders_count} limits:{limits_count}'.format(ts_code=ts_code, name=name,
-              circ_mv=int(misc[COL_CIRC_MV]), holders_count=int(misc[COL_HOLDERS_COUNT]), limits_count=int(misc[COL_CONTINUOUS_LIMIT_COUNT])),
+    plt.title('{ts_code} {name}'.format(ts_code=ts_code, name=name),
               fontproperties='Heiti TC')
     mpf.candlestick2_ochl(ax, df['open'], df['close'], df['high'], df['low'],
                           width=0.5, colorup='red', colordown='green',
