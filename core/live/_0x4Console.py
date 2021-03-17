08e9ec2a495a8ee9f7057a9cb80b74f8f2d004a1
vidalicious <weida.gu@yintech.cn>
2021-03-17 15:45:45 +0800

    dev

--- core/live/_0x4Console.py
+++ core/live/_0x4Console.py
@@ -7,8 +7,8 @@ from midas.core.data.engine import main_session
 import midas.core.data.models as models
 
 target_symbols = [
-    '603557',
-    '002498',
+    '002211',
+    '000007',
 ]
 
 target_symbols = list(set(target_symbols))
