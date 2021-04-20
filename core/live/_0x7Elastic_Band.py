71c4c3266ade419d9dd61db9d7b2237823b35994
vidalicious <weida.gu@yintech.cn>
2021-04-20 16:45:28 +0800

    dev

--- core/live/_0x7Elastic_Band.py
+++ core/live/_0x7Elastic_Band.py
@@ -93,17 +93,18 @@ def run():
                     today_open = float(j[1])
                     yesterday_closing_price = float(j[2])
                     current_price = float(j[3])
+                    today_high = float(j[4])
                     chg = (current_price / yesterday_closing_price - 1)
                     chg_display = '{}%'.format(round(chg*100, 2))
                     circ_mv = stock_map[code]['circ_mv']
-                    local_high = float(stock_map[code]['local_high'])
-                    fall_chg = (current_price / local_high - 1) * 100
+                    # local_high = float(stock_map[code]['local_high'])
+                    fall_chg = (current_price / today_high - 1) * 100
 
                     if_display = False
                     # if (chg > 0) and (current_price > today_open) and (-10 < fall_chg < 0):
                     #     if_display = True
 
-                    if (chg > 0) and (current_price > today_open):
+                    if (chg < 0) and (current_price < today_open):
                         if_display = True
 
                     if if_display:
@@ -111,10 +112,11 @@ def run():
                             'note': '{code}\t{name}\tchg:{chg}\tfall_chg:{fall_chg}%\tprice:{price}\tcirc_mv:{circ_mv}亿'.format(code=code, name=name, chg=chg_display,
                                 fall_chg=round(fall_chg, 2), price=round(current_price, 2), circ_mv=int(circ_mv)),
                             'chg': chg,
-                            'fall_chg': fall_chg
+                            'fall_chg': fall_chg,
+                            'circ_mv': circ_mv
                         })
 
-            displays.sort(key=lambda x: x['chg'], reverse=True)
+            displays.sort(key=lambda x: x['circ_mv'], reverse=True)
             # displays = displays[-10:]
             notes = [i['note'] for i in displays]
             print('\n'.join(notes))
