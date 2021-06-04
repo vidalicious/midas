32ea8aa3c0c4e98ce9b1af6022455f008af688da
vidalicious <weida.gu@yintech.cn>
2021-06-04 18:29:58 +0800

    dev

--- core/analyzer/test.py
+++ core/analyzer/test.py
@@ -25,6 +25,28 @@
 # a = pro.concept_detail(id='TS2', fields='ts_code,name')
 # pass
 
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set_theme(style="whitegrid")
+
+rs = np.random.RandomState(365)
+values = rs.randn(365, 4).cumsum(axis=0)
+dates = pd.date_range("1 1 2016", periods=365, freq="D")
+data = pd.DataFrame(values, dates, columns=["A", "B", "C", "D"])
+data = data.rolling(7).mean()
+
+sns.lineplot(data=data, palette="tab10", linewidth=2.5)
+
+import seaborn as sns
+
+flights = sns.load_dataset("flights")
+flights_wide = flights.pivot("year", "month", "passengers")
+flights_wide.head()
+sns.lineplot(data=flights_wide)
+
+
 a = [1, 2, 3, 4, 5, 6, 7]
 b = a[:50]
 
